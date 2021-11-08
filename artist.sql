19207b6f05ae7dd7e3175819a6d205844aee5fd5
Chris Kim <ckjr92@gmail.com>
2021-11-08 14:00:11 -0700

    finished employee.sql

--- artist.sql
+++ artist.sql
@@ -1,26 +1,26 @@
--- 1
+-- -- 1
 
-INSERT INTO artist (name)
-VALUES ('Twice'), ('BTS'), ('Blackpink');
+-- INSERT INTO artist (name)
+-- VALUES ('Twice'), ('BTS'), ('Blackpink');
 
--- 2
+-- -- 2
 
-SELECT * FROM artist
-ORDER BY name DESC
-LIMIT 10;
+-- SELECT * FROM artist
+-- ORDER BY name DESC
+-- LIMIT 10;
 
--- 3
+-- -- 3
 
-SELECT * FROM artist
-ORDER BY name 
-LIMIT 5;
+-- SELECT * FROM artist
+-- ORDER BY name 
+-- LIMIT 5;
 
--- 4
+-- -- 4
 
-SELECT name FROM artist
-WHERE name LIKE 'Black%';
+-- SELECT name FROM artist
+-- WHERE name LIKE 'Black%';
 
--- 5
+-- -- 5
 
-SELECT * FROM artist
-WHERE name LIKE '%Black%';
+-- SELECT * FROM artist
+-- WHERE name LIKE '%Black%';
---
+++ employee.sql
@@ -0,0 +1,22 @@
+-- 1
+
+SELECT first_name, last_name FROM employee
+WHERE city = 'Calgary';
+
+-- 2
+
+SELECT MIN(birth_date) FROM employee;
+
+-- 3
+
+SELECT MAX(birth_date) FROM employee;
+
+-- 4
+
+SELECT first_name, last_name from employee
+WHERE reports_to = 2;
+
+-- 5
+
+SELECT * FROM employee
+WHERE city = 'Lethbridge';
