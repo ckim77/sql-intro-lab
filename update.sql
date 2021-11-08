88501211cec893253d284057f2da400153d82e23
Chris Kim <ckjr92@gmail.com>
2021-11-08 14:52:59 -0700

    finished update.sql

--- update.sql
+++ update.sql
@@ -1,20 +1,24 @@
--- -- 1
--- update customer
--- set fax = null;
+-- 1
+update customer
+set fax = null;
 
--- -- 2
--- update customer 
--- set company = 'self'
--- where company is null;
+-- 2
+update customer 
+set company = 'self'
+where company is null;
 
--- -- 3
--- update customer
--- set last_name = 'Thompson'
--- where first_name = 'Julia';
+-- 3
+update customer
+set last_name = 'Thompson'
+where first_name = 'Julia';
 
--- -- 4
--- update customer
--- set support_rep_id = 4 where email = 'luisrojas@yahoo.cl';
+-- 4
+update customer
+set support_rep_id = 4 where email = 'luisrojas@yahoo.cl';
 
--- -- 5
--- select * from track
+-- 5
+select * from genre where name = 'Metal';
+update track
+set composer = 'The darkness around us'
+where composer is null and genre_id = 3;
+
