c3c11a39ab447984f272536a1dfd12a047ebc55b
Chris Kim <ckjr92@gmail.com>
2021-11-08 14:46:55 -0700

    finished everything but #5 on update due to a problem in the question

---
+++ update.sql
@@ -0,0 +1,20 @@
+-- -- 1
+-- update customer
+-- set fax = null;
+
+-- -- 2
+-- update customer 
+-- set company = 'self'
+-- where company is null;
+
+-- -- 3
+-- update customer
+-- set last_name = 'Thompson'
+-- where first_name = 'Julia';
+
+-- -- 4
+-- update customer
+-- set support_rep_id = 4 where email = 'luisrojas@yahoo.cl';
+
+-- -- 5
+-- select * from track
